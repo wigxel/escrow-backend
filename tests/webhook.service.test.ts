d512b9595fa9576324fa0783d3a2591a16662afe
kadima <williamsvgw@gmail.com>
2025-02-24 10:48:58 +0100

    TEST: webhook service - unsuccessful transfer event

--- tests/webhook.service.test.ts
+++ tests/webhook.service.test.ts
@@ -1,6 +1,9 @@
 import { Effect, Layer } from "effect";
 import { extendWithdrawalRepo } from "./mocks/withdrawalRepoMock";
-import { transferSuccessEvent, unsuccessfulTransferEvent } from "~/services/webhook.service";
+import {
+  transferSuccessEvent,
+  unsuccessfulTransferEvent,
+} from "~/services/webhook.service";
 import { TPaystackTransferWebhookEvent } from "~/utils/paystack/type/types";
 import { runTest } from "./mocks/app";
 import { extendTigerBeetleRepo } from "./mocks/tigerBeetleRepoMock";
@@ -69,4 +72,65 @@ describe("Webhook service", () => {
     });
   });
 
+  describe("Unsuccessful transfer event", () => {
+    const params = {
+      data: { status: "success", reference: "ref_CODE" },
+      event: "transfer.success",
+    } as TPaystackTransferWebhookEvent;
+
+    test("should fail if withdrawal reference code is invalid", () => {
+      const withdrawalRepo = extendWithdrawalRepo({
+        firstOrThrow() {
+          return Effect.fail(new Error(""));
+        },
+      });
+
+      const program = unsuccessfulTransferEvent(params);
+      const result = runTest(Effect.provide(program, withdrawalRepo));
+      expect(result).resolves.toMatchInlineSnapshot(
+        `[NoSuchElementException: Invalid withdrawal id]`,
+      );
+    });
+
+    test("should finalize unsuccessful transfer event", async () => {
+      let updateWithdrawal = false;
+      let createdTigerbeetleTransfer = false;
+      let deletedAccountStatement = false;
+      const withdrawalRepo = extendWithdrawalRepo({
+        update() {
+          updateWithdrawal = true;
+          return Effect.succeed([]);
+        },
+      });
+
+      const tigerBeetleRepo = extendTigerBeetleRepo({
+        createTransfers() {
+          createdTigerbeetleTransfer = true;
+          return Effect.succeed([]);
+        },
+      });
+
+      const accountStatementRepoMock = extendAccountStatementRepo({
+        delete() {
+          deletedAccountStatement = true;
+          return Effect.succeed([]);
+        },
+      });
+
+      const program = unsuccessfulTransferEvent(params);
+      const result = await runTest(
+        Effect.provide(
+          program,
+          withdrawalRepo.pipe(
+            Layer.provideMerge(tigerBeetleRepo),
+            Layer.provideMerge(accountStatementRepoMock),
+          ),
+        ),
+      );
+
+      expect(updateWithdrawal).toBeTruthy();
+      expect(createdTigerbeetleTransfer).toBeTruthy();
+      expect(deletedAccountStatement).toBeTruthy();
+    });
+  });
 });
