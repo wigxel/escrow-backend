811a1c073aa31c474c03950bcf7a4770b44a97b6
kadima <williamsvgw@gmail.com>
2025-02-24 01:30:57 +0100

    TEST: Escrow utility - get buyer and seller from participants

--- tests/escrowUtils.test.ts
+++ tests/escrowUtils.test.ts
@@ -1,4 +1,6 @@
-import { canTransitionEscrowStatus } from "~/services/escrow/escrow.utils";
+import { TEscrowParticipant } from "~/migrations/schema";
+import { canTransitionEscrowStatus, getBuyerAndSellerFromParticipants } from "~/services/escrow/escrow.utils";
+import { runTest } from "./mocks/app";
 
 describe("Escrow utility", () => {
   describe("Transition escrow status", () => {
@@ -25,5 +27,54 @@ describe("Escrow utility", () => {
         expect(res).toBeFalsy();
       }
     });
+  });
+
+  describe("Get buyer and seller from participants", () => {
+    const participants = [
+      {
+        id: "MOCK_ID",
+        escrowId: "MOCK_ESCROW_ID",
+        userId: "MOCK_BUYER_ID",
+        role: "buyer",
+        status: "active",
+      },
+      {
+        id: "MOCK_ID",
+        escrowId: "MOCK_ESCROW_ID",
+        userId: "MOCK_SELLER_ID",
+        role: "seller",
+        status: "active",
+      },
+    ] as TEscrowParticipant[];
+
+    test("should fail if buyer and seller not found", () => {
+      const program = getBuyerAndSellerFromParticipants([participants[0]])
+      const result = runTest(program)
+      expect(result).resolves.toMatchInlineSnapshot(`[ExpectedError: Invalid participants. Seller or buyer not found.]`)
+    });
+
+    test("should return participants", () => {
+      const program = getBuyerAndSellerFromParticipants(participants)
+      const result = runTest(program)
+      expect(result).resolves.toMatchInlineSnapshot(`
+        {
+          "buyer": {
+            "escrowId": "MOCK_ESCROW_ID",
+            "id": "MOCK_ID",
+            "role": "buyer",
+            "status": "active",
+            "userId": "MOCK_BUYER_ID",
+          },
+          "seller": {
+            "escrowId": "MOCK_ESCROW_ID",
+            "id": "MOCK_ID",
+            "role": "seller",
+            "status": "active",
+            "userId": "MOCK_SELLER_ID",
+          },
+        }
+      `)
+    });
+    
   });
 });
